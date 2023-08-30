e7c9918308b22dc0b9bf984b569125fb0e6acbfd
Stef Berben <stef@frostup.com>
2023-08-30 14:57:42 +0200

    swagger geconfigureerd

--- kanban-backend/src/main.ts
+++ kanban-backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Stef kanban')
+    .setDescription('Een mooie beschrijving')
+    .setVersion('1.0')
+    .addTag('kanban')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
